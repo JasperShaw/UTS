2ef9648b01970377a08f0c807266ff1af4180e53
Jasper Shaw <xiaoyongli01@live.com>
2020-03-17 12:29:24 +0800

    1229

---
+++ WpfApp1/WpfApp1/1228/1228Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1._1228
+{
+    class _1228Class
+    {
+        //试试看
+    }
+}
